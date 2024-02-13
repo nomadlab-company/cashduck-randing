6d054b7c4aa38b775a30892092ae4b04dfa85461
nomadlab-busan <122001657+nomadlab-busan@users.noreply.github.com>
2024-02-14 03:36:53 +0900

    다운로드 주소 수정

--- js/download.js
+++ js/download.js
@@ -25,5 +25,6 @@ function checkType() {
 }
 
 btnCheck.addEventListener('click', () => {
-  checkType();
+  // checkType();
+  window.location.href = `https://nomadlab.page.link?link=https://api.cashduck.kr/user/cashduck&apn=com.nomadlab.reward&amv=3&ibi=com.nomadlab.reward&isi=954086662896&ipbi=com.nomadlab.reward&imv=1&ifl=https://cashduck.kr/&ipfl=&https://cashduck.kr/ofl=https://cashduck.kr/`;
 });
